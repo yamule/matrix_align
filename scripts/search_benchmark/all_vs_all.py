c17d96a3767cfa7ba756d4fd82f08e1ac399ae8f
yamule <plant16@mogullao.sakura.ne.jp>
2024-09-25 18:08:18 +0900

    add benchmark script

--- scripts/search_benchmark/all_vs_all.py
+++ scripts/search_benchmark/all_vs_all.py
@@ -1,6 +1,7 @@
 import re,os,sys,gzip;
 import numpy as np;
 import argparse;
+import h5py;
 
 def check_bool(v):
     v = v.lower();
@@ -14,15 +15,13 @@ parser = argparse.ArgumentParser();
 parser.add_argument("--targetdir",required= True) ;
 parser.add_argument("--outdir",required=True) ;
 parser.add_argument("--rowwise_normalization",required=True) ;
-parser.add_argument("--function_type",help="correl, dist, dot, cos",required=True) ;
 
 args = parser.parse_args();
 
 targetdir = args.targetdir;
-function_type = args.function_type;
 rowwise_normalization = args.rowwise_normalization;
 outdir = args.outdir;
-if os.path.exists(outdir):
+if not os.path.exists(outdir):
     os.mkdir(outdir);
 
 def load_mat(infile):
@@ -106,8 +105,8 @@ if rowwise_normalization:
                     ssum[ii] += cc["value"][jj][ii];
 
     for ii in range(vsiz):
-        mmean.append(ssum[ii]/float(valcount));
-    
+        mmean[ii] = ssum[ii]/float(valcount);
+
     for aa in list(allfiles):
         c = load_mat(aa);
         for cc in c:
@@ -128,5 +127,107 @@ if rowwise_normalization:
     with open(statsfile,"wt") as fout:
         for ii in range(vsiz):
             fout.write(
-                "index:\t{}\tvar:\t{}\tmean:\t{}\n".format(ii,vvar[ii],mmean[ii])
+                "index:\t{}\tvar:\t{:.7f}\tmean:\t{:.7f}\n".format(ii,vvar[ii],mmean[ii])
             );
+
+vsiz = 0;
+h5table = None;
+h5out = None;
+numfiles = len(allfiles);
+name_desc = [];
+nsample_current = numfiles;
+sample_counter = 0;
+for ii in range(numfiles):
+    aa = allfiles[ii];
+    c = load_mat(aa);
+    if vsiz == 0:
+        vsiz = len(c[0]["value"][0]);
+        houtname = os.path.join(outdir,"temp.h5");
+        h5out = h5py.File(houtname,"w");
+        h5table = h5out.create_dataset("tmp", (nsample_current,vsiz),compression="gzip", maxshape=(None,vsiz));
+    for cc in list(c):
+        name_desc.append(
+            (cc["name"],re.split(r"[\s]+",cc["desc"])[0])
+        );
+        sample_counter += 1;
+        if sample_counter > nsample_current:
+            h5table.resize((sample_counter+100,vsiz));
+            nsample_current = sample_counter+100;
+        ssum = [0.0 for jj in range(vsiz)];
+        for vv in list(cc["value"]):
+            for vii in range(vsiz):
+                if stats is not None:
+                    if stats["std"][vii] == 0:
+                        ssum[vii] += vv[vii];
+                    else:
+                        ssum[vii] += (vv[vii]-stats["mean"][vii])/stats["std"][vii];
+                else:
+                    ssum[vii] += vv[vii];
+        for vii in range(vsiz):
+            ssum[vii] /= len(cc["value"]);
+        h5table[sample_counter -1,:] = np.array(ssum);
+h5out.close();
+
+h5in = h5py.File(houtname,"r");
+h5table = h5in["tmp"];
+
+def dot_product(a,b):
+    assert len(a.shape) == 1;
+    assert len(b.shape) == 1;
+    return (a*b).sum();
+
+def cos_dist(a,b):
+    assert len(a.shape) == 1;
+    assert len(b.shape) == 1;
+    anorm = np.sqrt((a*a).sum());
+    bnorm = np.sqrt((b*b).sum());
+    if anorm == 0 or bnorm == 0:
+        return 0.0;
+    return ((a/anorm)*(b/bnorm)).sum();
+
+def euc_dist(a,b):
+    assert len(a.shape) == 1;
+    assert len(b.shape) == 1;
+    c = a-b;
+    return np.sqrt((c*c).sum());
+
+def euc_dist_norm(a,b):
+    assert len(a.shape) == 1;
+    assert len(b.shape) == 1;
+    anorm = np.sqrt((a*a).sum());
+    bnorm = np.sqrt((b*b).sum());
+    c = (a/anorm)-(b/bnorm);
+    return np.sqrt((c*c).sum());
+
+def correl(a,b):
+    assert len(a.shape) == 1;
+    assert len(b.shape) == 1;
+    return np.corrcoef(a,b)[0,1];
+
+all_results = [];
+for ii in range(sample_counter):
+    query_name = name_desc[ii][0];
+    query_desc = name_desc[ii][1];
+    arr_i = h5table[ii];
+    
+    funcs = [
+        ("dot",dot_product),("cos_dist",cos_dist),("euc_dist",euc_dist),("euc_dist_norm",euc_dist_norm),("correl",correl)
+    ];
+    res = {};
+    for ff in list(funcs):
+        res[ff[0]] = [];
+    for jj in range(sample_counter):
+        if ii == jj:
+            continue;
+        arr_j = h5table[jj];
+        for tag,func in list(funcs):
+            res[tag].append((jj,func(arr_i,arr_j)));
+    for tag,func in list(funcs):
+        outname = os.path.join(outdir,"res_"+str(ii)+"."+tag+".dat");
+        with open(outname,"wt") as fout:
+            fout.write(">"+query_name+" "+query_desc+"\n");
+            t = list(sorted(res[tag],key=lambda x:x[1]));
+            for tt in list(t):
+                fout.write("\t".join(name_desc[tt[0]])+"\t"+str(tt[1])+"\n");
+h5in.close();
+os.remove(houtname);
