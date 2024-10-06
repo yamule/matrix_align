eb604e4b737557019f477d00ffd8cfd1398de471
yamule <1697130+yamule@users.noreply.github.com>
2024-10-06 19:55:57 +0900

    coded all_vs_all_toch.py

--- scripts/search_benchmark/all_vs_all.py
+++ scripts/search_benchmark/all_vs_all.py
@@ -1,7 +1,9 @@
 import re,os,sys,gzip;
 import numpy as np;
 import argparse;
-import h5py;
+import subprocess;
+
+EPSILON = 1.0e-7;
 
 def check_bool(v):
     v = v.lower();
@@ -16,6 +18,8 @@ parser.add_argument("--targetdir",required= True) ;
 parser.add_argument("--outdir",required=True) ;
 parser.add_argument("--global_per_channel_normalization",required=True,type=check_bool) ;
 parser.add_argument("--pigz",required=False,default=True,type=check_bool) ;
+parser.add_argument("--unbiased_global_stats",required=False,default=False,type=check_bool) ;
+
 
 args = parser.parse_args();
 
@@ -23,6 +27,8 @@ targetdir = args.targetdir;
 global_per_channel_normalization = args.global_per_channel_normalization;
 outdir = args.outdir;
 use_pigz = args.pigz;
+use_unbiased_global_stats = args.unbiased_global_stats;
+
 if not os.path.exists(outdir):
     os.mkdir(outdir);
 
@@ -40,12 +46,13 @@ def load_mat(infile):
             if ll.startswith("//"):
                 if current is not None:
                     ret.append(current);
+                current = None;
                 continue;
             if ll.startswith(">"):
                 if current is not None:
                     ret.append(current);
                     current = None;
-                mat = re.search(r">([^\s]+)[\s]+([^s].+)",ll);
+                mat = re.search(r">([^\s]+)[\s]+([^\s].+)",ll);
                 current = {};
                 if mat:
                     current["name"] = mat.group(1);
@@ -79,26 +86,30 @@ for aa in allfiles_:
         allfiles.append(
             os.path.join(targetdir,aa)
         );
+    else:
+        sys.stderr.write(aa+" was skipped.\n");
 stats = None;
 statsfile = os.path.join(outdir,"stats.dat");
 if global_per_channel_normalization:
     ssum = [];
     mmean = [];
     vvar = [];
-    vsiz = 0;
+    vsiz = None;
     headseq = None;
     valcount = 0;
     for aa in list(allfiles):
         c = load_mat(aa);
         if len(c) == 0:
             raise Exception(aa+" does not have data.");
-        if vsiz == 0:
+        if vsiz is None:
             vsiz = len(c[0]["value"][0]);
             headseq = c[0];
             for _ in range(vsiz):
                 ssum.append(0);
                 mmean.append(0);
                 vvar.append(0);
+        else:
+            assert len(c[0]["value"][0]) == vsiz, "Inconsistent value sizes detected."+aa+"\n";
         for cc in c:
             for jj in range(len(cc["value"])):
                 valcount += 1;
@@ -117,7 +128,10 @@ if global_per_channel_normalization:
                     vvar[ii] += (mmean[ii]-cc["value"][jj][ii])*(mmean[ii]-cc["value"][jj][ii]);
     sstd = [];
     for ii in range(vsiz):
-        vvar[ii] /= valcount;
+        if use_unbiased_global_stats:
+            vvar[ii] /= valcount-1;
+        else:
+            vvar[ii] /= valcount;
         sstd.append(
             np.sqrt(vvar[ii])
         );
@@ -132,41 +146,60 @@ if global_per_channel_normalization:
                 "index:\t{}\tvar:\t{:.7f}\tmean:\t{:.7f}\n".format(ii,vvar[ii],mmean[ii])
             );
 
-vsiz = 0;
-allvalues = [];
+allvalues_average = [];
+allvalues_max = [];
+allvalues_min = [];
 numfiles = len(allfiles);
 name_desc = [];
-nsample_current = numfiles;
+
 sample_counter = 0;
 for ii in range(numfiles):
     aa = allfiles[ii];
     c = load_mat(aa);
-    if vsiz == 0:
+    if vsiz is None:
         vsiz = len(c[0]["value"][0]);
-        
+    else:
+        assert len(c[0]["value"][0]) == vsiz, "Inconsistent value sizes detected."+aa+"\n";
+
     for cc in list(c):
         name_desc.append(
             (cc["name"],re.split(r"[\s]+",cc["desc"])[0])
         );
         sample_counter += 1;
         ssum = [0.0 for jj in range(vsiz)];
+        mmax = [None for jj in range(vsiz)];
+        mmin = [None for jj in range(vsiz)];
         for vv in list(cc["value"]):
             for vii in range(vsiz):
+                
                 if stats is not None:
                     if stats["std"][vii] == 0:
-                        ssum[vii] += vv[vii];
+                        xvalue = 0.0;
                     else:
-                        ssum[vii] += (vv[vii]-stats["mean"][vii])/stats["std"][vii];
+                        xvalue = (vv[vii]-stats["mean"][vii])/stats["std"][vii];
                 else:
-                    ssum[vii] += vv[vii];
+                    xvalue = vv[vii];
+                
+                ssum[vii] += xvalue;
+                if mmax[vii] is None:
+                    mmax[vii] = xvalue;
+                    mmin[vii] = xvalue;
+                else:
+                    mmax[vii] = max([mmax[vii],xvalue]);
+                    mmin[vii] = min([mmin[vii],xvalue]);
+
         for vii in range(vsiz):
             ssum[vii] /= len(cc["value"]);
-        allvalues.append(np.array(ssum));
+        allvalues_average.append(np.array(ssum));
+        allvalues_max.append(np.array(mmax));
+        allvalues_min.append(np.array(mmin));
 
-houtname = os.path.join(outdir,"averaged.dat");
-with open(houtname,"wt") as fout:
-    for ii in range(sample_counter):
-        fout.write("\t".join(name_desc[ii])+"\t"+"\t".join(["{:.7f}".format(x) for x in allvalues[ii].tolist()])+"\n");
+for (stag,val) in [("average",allvalues_average),("max",allvalues_max),("min",allvalues_min)]:
+    houtname = os.path.join(outdir,stag+".dat");
+    with open(houtname,"wt") as fout:
+        for ii in range(sample_counter):
+            fout.write("\t".join(name_desc[ii])+"\t"+"\t".join(["{:.7f}".format(x) for x in val[ii].tolist()])+"\n");
+
 
 def dot_product(a,b):
     assert len(a.shape) == 1;
@@ -176,10 +209,8 @@ def dot_product(a,b):
 def cos_sim(a,b):
     assert len(a.shape) == 1;
     assert len(b.shape) == 1;
-    anorm = np.sqrt((a*a).sum());
-    bnorm = np.sqrt((b*b).sum());
-    if anorm == 0 or bnorm == 0:
-        return 0.0;
+    anorm = np.sqrt((a*a).sum())+EPSILON;
+    bnorm = np.sqrt((b*b).sum())+EPSILON;
     return ((a/anorm)*(b/bnorm)).sum();
 
 def euc_dist(a,b):
@@ -191,40 +222,44 @@ def euc_dist(a,b):
 def euc_dist_norm(a,b):
     assert len(a.shape) == 1;
     assert len(b.shape) == 1;
-    anorm = np.sqrt((a*a).sum());
-    bnorm = np.sqrt((b*b).sum());
+    anorm = np.sqrt((a*a).sum())+EPSILON;
+    bnorm = np.sqrt((b*b).sum())+EPSILON;
     c = (a/anorm)-(b/bnorm);
     return np.sqrt((c*c).sum());
 
 def correl(a,b):
     assert len(a.shape) == 1;
     assert len(b.shape) == 1;
+
+    if np.all(a == a[0]) or np.all(b == b[0]):
+        return 0.0;
+
     return np.corrcoef(a,b)[0,1];
 
-all_results = [];
-for ii in range(sample_counter):
-    query_name = name_desc[ii][0];
-    query_desc = name_desc[ii][1];
-    arr_i = allvalues[ii];
-    
-    funcs = [
-        ("dot",dot_product),("cos_sim",cos_sim),("euc_dist",euc_dist),("euc_dist_norm",euc_dist_norm),("correl",correl)
-    ];
-    res = {};
-    for ff in list(funcs):
-        res[ff[0]] = [];
-    for jj in range(sample_counter):
-        if ii == jj:
-            continue;
-        arr_j = allvalues[jj];
+for (stag,allvalues) in [("average",allvalues_average),("max",allvalues_max),("min",allvalues_min)]:
+    for ii in range(sample_counter):
+        query_name = name_desc[ii][0];
+        query_desc = name_desc[ii][1];
+        arr_i = allvalues[ii];
+        
+        funcs = [
+            ("dot",dot_product),("cos_sim",cos_sim),("euc_dist",euc_dist),("euc_dist_norm",euc_dist_norm),("correl",correl)
+        ];
+        res = {};
+        for ff in list(funcs):
+            res[ff[0]] = [];
+        for jj in range(sample_counter):
+            if ii == jj:
+                continue;
+            arr_j = allvalues[jj];
+            for tag,func in list(funcs):
+                res[tag].append((jj,func(arr_i,arr_j)));
         for tag,func in list(funcs):
-            res[tag].append((jj,func(arr_i,arr_j)));
-    for tag,func in list(funcs):
-        outname = os.path.join(outdir,"res_"+str(ii)+"."+tag+".dat");
-        with open(outname,"wt") as fout:
-            fout.write(">"+query_name+" "+query_desc+"\n");
-            t = list(sorted(res[tag],key=lambda x:x[1]));
-            for tt in list(t):
-                fout.write("\t".join(name_desc[tt[0]])+"\t"+"{:.7f}".format(tt[1])+"\n");
-        if use_pigz:
-            os.system("pigz "+outname)
+            outname = os.path.join(outdir,"res_"+str(ii)+"."+stag+"."+tag+".dat");
+            with open(outname,"wt") as fout:
+                fout.write(">"+query_name+" "+query_desc+"\n");
+                t = list(sorted(res[tag],key=lambda x:x[1]));
+                for tt in list(t):
+                    fout.write("\t".join(name_desc[tt[0]])+"\t"+"{:.7f}".format(tt[1])+"\n");
+            if use_pigz:
+                subprocess.run(["pigz",outname],check=True);
--- scripts/search_benchmark/all_vs_all_torch.py
+++ scripts/search_benchmark/all_vs_all_torch.py
@@ -1,7 +1,12 @@
 import re,os,sys,gzip;
 import numpy as np;
 import argparse;
-import h5py;
+import subprocess;
+import torch;
+import math;
+import shutil;
+
+EPSILON = 1.0e-7;
 
 def check_bool(v):
     v = v.lower();
@@ -14,8 +19,11 @@ def check_bool(v):
 parser = argparse.ArgumentParser();
 parser.add_argument("--targetdir",required= True) ;
 parser.add_argument("--outdir",required=True) ;
+parser.add_argument("--device",required=False,default="cuda") ;
 parser.add_argument("--global_per_channel_normalization",required=True,type=check_bool) ;
 parser.add_argument("--pigz",required=False,default=True,type=check_bool) ;
+parser.add_argument("--batch_size",required=False,default=20,type=int) ;
+parser.add_argument("--unbiased_global_stats",required=False,default=False,type=check_bool) ;
 
 args = parser.parse_args();
 
@@ -23,6 +31,18 @@ targetdir = args.targetdir;
 global_per_channel_normalization = args.global_per_channel_normalization;
 outdir = args.outdir;
 use_pigz = args.pigz;
+use_unbiased_global_stats = args.unbiased_global_stats;
+batch_size=args.batch_size;
+ddev = torch.device(args.device);
+
+EPSILON_TENSOR=torch.tensor(EPSILON, dtype=torch.float32, device=ddev)
+
+if use_pigz and shutil.which("pigz") is None:
+    raise Exception("pigz is not found.");
+
+if not torch.cuda.is_available() and ddev.type == 'cuda':
+    raise Exception("CUDA is not available.");
+
 if not os.path.exists(outdir):
     os.mkdir(outdir);
 
@@ -40,12 +60,13 @@ def load_mat(infile):
             if ll.startswith("//"):
                 if current is not None:
                     ret.append(current);
+                current = None;
                 continue;
             if ll.startswith(">"):
                 if current is not None:
                     ret.append(current);
                     current = None;
-                mat = re.search(r">([^\s]+)[\s]+([^s].+)",ll);
+                mat = re.search(r">([^\s]+)[\s]+([^\s].+)",ll);
                 current = {};
                 if mat:
                     current["name"] = mat.group(1);
@@ -79,26 +100,33 @@ for aa in allfiles_:
         allfiles.append(
             os.path.join(targetdir,aa)
         );
+    else:
+        sys.stderr.write(aa+" was skipped.\n");
 stats = None;
-statsfile = os.path.join(outdir,"stats.dat");
+if use_unbiased_global_stats:
+    statsfile = os.path.join(outdir,"stats.unbiased.dat");
+else:
+    statsfile = os.path.join(outdir,"stats.dat");
 if global_per_channel_normalization:
     ssum = [];
     mmean = [];
     vvar = [];
-    vsiz = 0;
+    vsiz = None;
     headseq = None;
     valcount = 0;
     for aa in list(allfiles):
         c = load_mat(aa);
         if len(c) == 0:
             raise Exception(aa+" does not have data.");
-        if vsiz == 0:
+        if vsiz is None:
             vsiz = len(c[0]["value"][0]);
             headseq = c[0];
             for _ in range(vsiz):
                 ssum.append(0);
                 mmean.append(0);
                 vvar.append(0);
+        else:
+            assert len(c[0]["value"][0]) == vsiz, "Inconsistent value sizes detected."+aa+"\n";
         for cc in c:
             for jj in range(len(cc["value"])):
                 valcount += 1;
@@ -117,7 +145,10 @@ if global_per_channel_normalization:
                     vvar[ii] += (mmean[ii]-cc["value"][jj][ii])*(mmean[ii]-cc["value"][jj][ii]);
     sstd = [];
     for ii in range(vsiz):
-        vvar[ii] /= valcount;
+        if use_unbiased_global_stats:
+            vvar[ii] /= valcount-1;
+        else:
+            vvar[ii] /= valcount;
         sstd.append(
             np.sqrt(vvar[ii])
         );
@@ -129,104 +160,149 @@ if global_per_channel_normalization:
     with open(statsfile,"wt") as fout:
         for ii in range(vsiz):
             fout.write(
-                "index:\t{}\tvar:\t{:.7f}\tmean:\t{:.7f}\n".format(ii,vvar[ii],mmean[ii])
+                "index:\t{}\tvar:\t{:.7f}\tmean:\t{:.7f}\tcount:\t{}\n".format(ii,vvar[ii],mmean[ii],valcount)
             );
 
-vsiz = 0;
-allvalues = [];
+allvalues_average = [];
+allvalues_max = [];
+allvalues_min = [];
 numfiles = len(allfiles);
 name_desc = [];
-nsample_current = numfiles;
+
 sample_counter = 0;
 for ii in range(numfiles):
     aa = allfiles[ii];
     c = load_mat(aa);
-    if vsiz == 0:
+    if vsiz is None:
         vsiz = len(c[0]["value"][0]);
-        
+    else:
+        assert len(c[0]["value"][0]) == vsiz, "Inconsistent value sizes detected."+aa+"\n";
+
     for cc in list(c):
         name_desc.append(
-            (cc["name"],re.split(r"[\s]+",cc["desc"])[0])
+            (cc["name"],re.split(r"[\s]+",cc["desc"])[0]) # 最初のカラムに Family ID が入っている想定
         );
         sample_counter += 1;
         ssum = [0.0 for jj in range(vsiz)];
+        mmax = [None for jj in range(vsiz)];
+        mmin = [None for jj in range(vsiz)];
         for vv in list(cc["value"]):
             for vii in range(vsiz):
+                
                 if stats is not None:
                     if stats["std"][vii] == 0:
-                        ssum[vii] += vv[vii];
+                        xvalue = vv[vii]-stats["mean"][vii];
                     else:
-                        ssum[vii] += (vv[vii]-stats["mean"][vii])/stats["std"][vii];
+                        xvalue = (vv[vii]-stats["mean"][vii])/stats["std"][vii];
                 else:
-                    ssum[vii] += vv[vii];
+                    xvalue = vv[vii];
+                
+                ssum[vii] += xvalue;
+                if mmax[vii] is None:
+                    mmax[vii] = xvalue;
+                    mmin[vii] = xvalue;
+                else:
+                    mmax[vii] = max([mmax[vii],xvalue]);
+                    mmin[vii] = min([mmin[vii],xvalue]);
+
         for vii in range(vsiz):
             ssum[vii] /= len(cc["value"]);
-        allvalues.append(np.array(ssum));
+        allvalues_average.append(ssum);
+        allvalues_max.append(mmax);
+        allvalues_min.append(mmin);
 
-houtname = os.path.join(outdir,"averaged.dat");
-with open(houtname,"wt") as fout:
-    for ii in range(sample_counter):
-        fout.write("\t".join(name_desc[ii])+"\t"+"\t".join(["{:.7f}".format(x) for x in allvalues[ii].tolist()])+"\n");
+
+for (stag,val) in [("average",allvalues_average),("max",allvalues_max),("min",allvalues_min)]:
+    houtname = os.path.join(outdir,stag+".dat");
+    with open(houtname,"wt") as fout:
+        for ii in range(sample_counter):
+            fout.write("\t".join(name_desc[ii])+"\t"+"\t".join(["{:.7f}".format(float(x)) for x in val[ii]])+"\n");
+
+allvalues_average = torch.tensor(allvalues_average,dtype=torch.float32,device=ddev);
+allvalues_max = torch.tensor(allvalues_max,dtype=torch.float32,device=ddev);
+allvalues_min = torch.tensor(allvalues_min,dtype=torch.float32,device=ddev);
 
 def dot_product(a,b):
     assert len(a.shape) == 2;
     assert len(b.shape) == 2;
-    return (a*b).sum();
+    return (a*b).sum(dim=-1);
 
 def cos_sim(a,b):
     assert len(a.shape) == 2;
     assert len(b.shape) == 2;
-    anorm = np.sqrt((a*a).sum());
-    bnorm = np.sqrt((b*b).sum());
-    if anorm == 0 or bnorm == 0:
-        return 0.0;
-    return ((a/anorm)*(b/bnorm)).sum();
+    anorm = torch.sqrt((a*a).sum(dim=-1, keepdim=True));
+    bnorm = torch.sqrt((b*b).sum(dim=-1, keepdim=True));
+    amask = anorm > 0.0;
+    bmask = bnorm > 0.0;
+    anorm = torch.where(amask, anorm, EPSILON_TENSOR);
+    bnorm = torch.where(bmask, bnorm,EPSILON_TENSOR);
+    return torch.where(amask*bmask,((a/anorm)*(b/bnorm)).sum(dim=-1), torch.tensor(0.0, dtype=torch.float32, device=ddev));
 
 def euc_dist(a,b):
     assert len(a.shape) == 2;
     assert len(b.shape) == 2;
     c = a-b;
-    return np.sqrt((c*c).sum());
+    return torch.sqrt((c*c).sum(dim=-1));
 
 def euc_dist_norm(a,b):
     assert len(a.shape) == 2;
     assert len(b.shape) == 2;
-    anorm = np.sqrt((a*a).sum());
-    bnorm = np.sqrt((b*b).sum());
+    anorm = torch.sqrt((a*a).sum(dim=-1, keepdim=True));
+    bnorm = torch.sqrt((b*b).sum(dim=-1, keepdim=True));
+    anorm = torch.where(anorm == 0.0, EPSILON_TENSOR, anorm);
+    bnorm = torch.where(bnorm == 0.0, EPSILON_TENSOR, bnorm);
     c = (a/anorm)-(b/bnorm);
-    return np.sqrt((c*c).sum());
+    return torch.sqrt((c*c).sum(dim=-1));
 
+def correl(a, b):
+    assert len(a.shape) == 2;
+    assert len(b.shape) == 2;
+    mean_a = a.mean(dim=1, keepdim=True);
+    mean_b = b.mean(dim=1, keepdim=True);
+    a_centered = a - mean_a;
+    b_centered = b - mean_b;
+    numerator = (a_centered * b_centered).sum(dim=1);
+    denominator = torch.sqrt((a_centered ** 2).sum(dim=1) * (b_centered ** 2).sum(dim=1));
+    zero_denominator = denominator == 0;
+    denominator = torch.where(zero_denominator, EPSILON_TENSOR, denominator);
+    return torch.where(zero_denominator, torch.tensor(0.0, dtype=torch.float32, device=ddev),  numerator / denominator);
 
-def correl_(a,b):
-    assert len(a.shape) == 1;
-    assert len(b.shape) == 1;
-    return torch.corrcoef(a,b)[0,1];
-
-かきさし
-all_results = [];
-for ii in range(sample_counter):
-    query_name = name_desc[ii][0];
-    query_desc = name_desc[ii][1];
-    arr_i = allvalues[ii];
     
-    funcs = [
-        ("dot",dot_product),("cos_sim",cos_sim),("euc_dist",euc_dist),("euc_dist_norm",euc_dist_norm),("correl",correl)
-    ];
-    res = {};
-    for ff in list(funcs):
-        res[ff[0]] = [];
-    for jj in range(sample_counter):
-        if ii == jj:
-            continue;
-        arr_j = allvalues[jj];
-        for tag,func in list(funcs):
-            res[tag].append((jj,func(arr_i,arr_j)));
-    for tag,func in list(funcs):
-        outname = os.path.join(outdir,"res_"+str(ii)+"."+tag+".dat");
-        with open(outname,"wt") as fout:
-            fout.write(">"+query_name+" "+query_desc+"\n");
-            t = list(sorted(res[tag],key=lambda x:x[1]));
-            for tt in list(t):
-                fout.write("\t".join(name_desc[tt[0]])+"\t"+"{:.7f}".format(tt[1])+"\n");
-        if use_pigz:
-            os.system("pigz "+outname)
+for (stag,allvalues) in [("average",allvalues_average),("max",allvalues_max),("min",allvalues_min)]:
+    for ii in range(sample_counter):
+        query_name = name_desc[ii][0];
+        query_desc = name_desc[ii][1];
+        arr_i = allvalues[ii];
+        
+        funcs = [
+            ("dot",dot_product,True),("cos_sim",cos_sim,True),("euc_dist",euc_dist,False),("euc_dist_norm",euc_dist_norm,False),("correl",correl,True)
+        ];
+        res = {};
+        for ff in list(funcs):
+            res[ff[0]] = [];
+
+        num_batches = math.ceil(sample_counter/batch_size);
+        arr_i_expanded = arr_i.unsqueeze(0).repeat(batch_size, 1);
+
+        for jj in range(num_batches):
+            end_index = min((jj+1)*batch_size, sample_counter);
+            arr_j = allvalues[jj*batch_size:end_index];
+            current_siz = arr_j.shape[0];
+            for tag,func in list(funcs):
+                with torch.no_grad():
+                    batch_res = func(arr_i_expanded[:current_siz],arr_j).detach().cpu().tolist();
+                for kkk in range(current_siz):
+                    globalindex = jj*batch_size+kkk;
+                    if globalindex == ii:
+                        continue;
+                    res[tag].append((globalindex,batch_res[kkk]));
+
+        for tag,func,reverser in list(funcs):
+            outname = os.path.join(outdir,"res_"+str(ii)+"."+stag+"."+tag+".dat");
+            with open(outname,"wt") as fout:
+                fout.write(">"+query_name+" "+query_desc+"\n");
+                t = list(sorted(res[tag],key=lambda x:x[1],reverse=reverser));
+                for tt in list(t):
+                    fout.write("\t".join(name_desc[tt[0]])+"\t"+"{:.7f}".format(tt[1])+"\n");
+            if use_pigz:
+                subprocess.run(["pigz",outname],check=True);
