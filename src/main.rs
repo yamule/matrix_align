26be75a91cf3d8c80c7e5ad6197c84be7a0a1a9f
yamule <plant16@mogullao.sakura.ne.jp>
2025-12-16 19:04:14 +0900

    Update help message.

--- src/main.rs
+++ src/main.rs
@@ -209,6 +209,7 @@ fn main_(mut args:Vec<String>){
 
     for aa in args.iter(){
         if aa == "--help" || aa == "-h"{
+            eprintln!("GPSMalign: Matrix based multiple sequence alignment tool.");
             eprintln!("Version: dev_20251216");
             matrix_process::check_simd();
         }
